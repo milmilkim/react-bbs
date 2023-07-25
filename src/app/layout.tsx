853862a65fa3336ffaa4f6247da13c1c8380c3e4
milmilkim <soundkim27@gmail.com>
2023-07-26 02:07:10 +0900

    feat: config 확인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import {
   query,
   ref,
 } from 'firebase/database';
+import config from '../../firebase.config'
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -21,7 +22,7 @@ export const metadata: Metadata = {
 const getData = () => {
   return new Promise((resolve, reject) => {
     const userRef = ref(database, 'users/');
-    const adminUserRef = query(userRef, orderByChild('role'), equalTo('admin'));
+    const adminUserRef = query(userRef, orderByChild('role'), equalTo('user'));
 
     onValue(adminUserRef, (snapshot) => {
       if (snapshot.exists()) {
@@ -47,7 +48,10 @@ export default async function RootLayout({
           <>{children}</>
         ) : (
           <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            관리자 계정이 존재하지 않습니다. 관리자 계정을 추가하세요.
+           <div>
+            <p>관리자 계정이 존재하지 않습니다. 관리자 계정을 추가하세요.</p>
+            <p>{JSON.stringify(config)}</p>
+            </div>
           </main>
         )}
       </body>

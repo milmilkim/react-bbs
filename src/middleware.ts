4f38b7d864aee14d838d33f468ee9727a6ee56b1
milmilkim <soundkim27@gmail.com>
2023-08-01 11:12:22 +0900

    fix: secret 환경변수로 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { type NextRequest, NextResponse } from 'next/server';
 export async function middleware(req: NextRequest) {
   // 로그인 중일 땐 로그인 페이지 접근 안 됨
   if (req.nextUrl.pathname.startsWith('/auth')) {
-    const token = await getToken({ req, secret: 'secret', raw: false });
+    const token = await getToken({ req, secret: process.env.SECRET, raw: false });
     if (token) {
       return NextResponse.redirect(new URL('/', req.url));
     }
@@ -12,7 +12,8 @@ export async function middleware(req: NextRequest) {
 
   // 어드민 권한 체크
   if (req.nextUrl.pathname.startsWith('/admin')) {
-    const token = await getToken({ req, secret: 'secret', raw: false });
+    const token = await getToken({ req, secret: process.env.SECRET, raw: false });
+    console.log(token)
 
     if(!token) {
       return NextResponse.redirect(new URL('/auth/login', req.url));
